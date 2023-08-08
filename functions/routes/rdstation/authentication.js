be8c44b6b9c75fed9493c3ae373fd2fa156e99de
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-08-08 17:15:41 -0300

    chore: accept get in url and redirect to app

--- functions/routes/rdstation/authentication.js
+++ functions/routes/rdstation/authentication.js
@@ -2,7 +2,7 @@ const getAppData = require('./../../lib/store-api/get-app-data')
 const updateAppData = require('./../../lib/store-api/update-app-data')
 
 exports.post = ({ appSdk, admin }, req, res) => {
-  console.log('>>Webhook RD: ')
+  console.log('>>Webhook RD: POST')
   const { body, query } = req
   let { storeId, code } = query
   storeId = parseInt(storeId, 10)
@@ -110,5 +110,16 @@ exports.post = ({ appSdk, admin }, req, res) => {
       status: 404,
       msg: `StoreId #${storeId} not found`
     })
+  }
+}
+
+exports.get = ({ appSdk, admin }, req, res) => {
+  console.log('>>Webhook RD: GET')
+  const { body, query } = req
+  let { storeId, code } = query
+  storeId = parseInt(storeId, 10)
+  console.log('>> Store: ', storeId, ' body: ', JSON.stringify(body), ' <<')
+  if (storeId > 100) {
+    res.status(200).redirect('https://app.e-com.plus/#/apps/edit/111968/')
   }
 }
